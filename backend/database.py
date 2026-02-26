2258bf737865504dd96329b0ec6d9c727c24faf3
Norbertx23 <norbertx23@gmail.com>
2026-02-26 18:32:58 +0100

    dalsze poprawki

--- backend/database.py
+++ backend/database.py
@@ -9,10 +9,12 @@ from urllib.parse import quote_plus
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    db_password = os.getenv("DB_PASSWORD", "postgres")
-    db_user = os.getenv("DB_USER", "postgres")
-    db_name = os.getenv("DB_NAME", "battleship_db")
+    db_user = os.getenv("POSTGRES_USER")
+    db_password = os.getenv("POSTGRES_PASSWORD")
+    db_name = os.getenv("POSTGRES_DB")
     db_host = os.getenv("DB_HOST", "localhost")
+    if not all([db_user, db_password, db_name]):
+        raise ImportError("Błąd: Brak wymaganych zmiennych POSTGRES_ w pliku .env!")
     DATABASE_URL = f"postgresql://{quote_plus(db_user)}:{quote_plus(db_password)}@{db_host}:5432/{db_name}"
 
 engine = create_engine(DATABASE_URL)
